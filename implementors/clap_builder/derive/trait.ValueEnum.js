1fef66445cf112145f506a63af6f2a5cd32379ec
GitHub Action <action@github.com>
2024-03-26 20:52:02 +0000

    deploy

---
+++ implementors/clap_builder/derive/trait.ValueEnum.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"netadm":[["impl ValueEnum for <a class=\"enum\" href=\"netadm/enum.V4Mode.html\" title=\"enum netadm::V4Mode\">V4Mode</a>"],["impl ValueEnum for <a class=\"enum\" href=\"netadm/enum.V6Mode.html\" title=\"enum netadm::V6Mode\">V6Mode</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,5 @@
+var sourcesIndex = JSON.parse('{\
+"libnet":["",[],["ioctl.rs","ip.rs","kstat.rs","lib.rs","link.rs","ndpd.rs","nvlist.rs","route.rs","sys.rs"]],\
+"netadm":["",[],["main.rs"]]\
+}');
+createSourceSidebar();
