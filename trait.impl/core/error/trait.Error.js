57e77c3ec8e9c3e4bd3fad7eeb1188a3ff5e87c1
GitHub Action <action@github.com>
2025-06-18 04:15:24 +0000

    deploy

---
+++ trait.impl/core/error/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["libnet",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"libnet/enum.Error.html\" title=\"enum libnet::Error\">Error</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"libnet/pf_key/enum.Error.html\" title=\"enum libnet::pf_key::Error\">Error</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"libnet/route/enum.Error.html\" title=\"enum libnet::route::Error\">Error</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[748]}
